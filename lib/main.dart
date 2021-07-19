27b66d52e33026ae47e3d1f29b94b81e7d6cfab4
Cali0707 <calumramurray@gmail.com>
2021-07-19 10:42:08 -0400

    Finished setting up firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,42 @@
 import 'package:flutter/material.dart';
 import 'src/pages/home_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
+
+class App extends StatefulWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _initialization,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          //TODO: add an error page
+        }
+
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MyApp();
+        }
+
+        //TODO: add a loading page
+      },
+    );
+  }
+}
+
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
