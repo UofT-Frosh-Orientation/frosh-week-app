a656ea5888fc268093da3c0ea1d8c14d90c061b2
Cali0707 <calumramurray@gmail.com>
2021-08-18 22:03:05 -0400

    Refactored

---
+++ lib/framework.dart
@@ -0,0 +1,213 @@
+import 'package:animations/animations.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart' as fss;
+import 'package:frosh_week_2t1/src/pages/home_page.dart';
+import 'package:frosh_week_2t1/src/pages/leaders_page.dart';
+import 'package:frosh_week_2t1/src/pages/login_page.dart';
+import 'package:frosh_week_2t1/src/pages/notifications_page.dart';
+import 'package:frosh_week_2t1/src/pages/resources_page.dart';
+import 'package:frosh_week_2t1/src/pages/schedule_page.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'src/colors.dart';
+
+
+Future<bool> hasNetwork() async {
+  ConnectivityResult connectivityResult = await (Connectivity().checkConnectivity());
+  return connectivityResult != ConnectivityResult.none;
+}
+
+class Framework extends StatefulWidget {
+  final bool isLoggedIn;
+  final Dio dio;
+  final fss.FlutterSecureStorage storage;
+  final dynamic loadedData;
+
+  const Framework({
+    Key? key,
+    required this.isLoggedIn,
+    required this.dio,
+    required this.storage,
+    required this.loadedData,
+  }) : super(key: key);
+
+  @override
+  FrameworkState createState() => FrameworkState();
+}
+
+class FrameworkState extends State<Framework> {
+  late PageController pageController;
+  int selectedIndex = 0;
+  late bool _loggedIn;
+  String froshName = "";
+  String froshGroup = "";
+  String froshId = "";
+  String discipline = "";
+  String shirtSize = "";
+
+  Future<void> setLoggedIn(bool login) async {
+    setState(() {
+      _loggedIn = login;
+    });
+    if (login) {
+      await getCurrentUser();
+    }
+  }
+
+  Future<bool> getCurrentUser() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    if (await hasNetwork()) {
+      print("Has network");
+      String? cookie = await widget.storage.read(key: 'cookie');
+      Response res = await widget.dio
+          .get('https://www.orientation.skule.ca/users/current',
+          options: Options(headers: {"cookie": cookie}))
+          .catchError((error) {
+        setState(() {
+          _loggedIn = false;
+        });
+      });
+      setState(() {
+        froshName = res.data["preferredName"];
+        froshGroup = res.data["froshGroup"];
+        froshId = res.data["_id"];
+        discipline = res.data["discipline"];
+        shirtSize = res.data["shirtSize"];
+      });
+      await prefs.setStringList(
+          'froshData', [froshName, froshGroup, froshId, discipline, shirtSize]);
+      return true;
+    } else {
+      List<String>? froshData = prefs.getStringList("froshData");
+      setState(() {
+        froshName = froshData![0];
+        froshGroup = froshData[1];
+        froshId = froshData[2];
+        discipline = froshData[3];
+        shirtSize = froshData[4];
+      });
+      return true;
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    pageController = PageController(initialPage: selectedIndex);
+    setLoggedIn(widget.isLoggedIn);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    bool leaderAccount = true;
+
+    List<Widget> pages = [
+      HomePage(
+        froshName: froshName,
+        froshGroup: froshGroup,
+        froshId: froshId,
+        discipline: discipline,
+        shirtSize: shirtSize,
+        froshScheduleData: widget.loadedData["scheduleJSON"]["lambda"],
+        welcomeMessage: widget.loadedData["welcomeMessage"],
+      ),
+      SchedulePage(data: widget.loadedData["scheduleJSON"]["lambda"]),
+      NotificationsPageParse(),
+      ResourcesPageParse(),
+    ];
+    List<Widget> icons = [
+      Icon(
+        Icons.home,
+        size: 30,
+        color: Colors.white,
+      ),
+      Icon(
+        Icons.event,
+        size: 30,
+        color: Colors.white,
+      ),
+      Icon(
+        Icons.notifications,
+        size: 30,
+        color: Colors.white,
+      ),
+      Icon(
+        Icons.book,
+        size: 30,
+        color: Colors.white,
+      ),
+    ];
+
+    if (leaderAccount) {
+      pages.add(LeadersPage());
+      icons.add(
+        Icon(
+          Icons.people,
+          size: 30,
+          color: Colors.white,
+        ),
+      );
+    }
+
+    if (!_loggedIn) {
+      return Scaffold(
+          body: LoginPage(
+              dio: widget.dio,
+              setLoggedIn: setLoggedIn,
+              storage: widget.storage));
+    } else
+      return Scaffold(
+        body: Stack(children: [
+          PageTransitionSwitcher(
+            transitionBuilder: (
+                child,
+                animation,
+                secondaryAnimation,
+                ) {
+              return FadeThroughTransition(
+                animation: animation,
+                secondaryAnimation: secondaryAnimation,
+                child: child,
+              );
+            },
+            child: pages[selectedIndex],
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: 80,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment(0, 0.8),
+                  colors: [
+                    Theme.of(this.context).canvasColor.withOpacity(0),
+                    Theme.of(this.context).colorScheme.lightPurpleAccent,
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: CurvedNavigationBar(
+              buttonBackgroundColor:
+              Theme.of(this.context).colorScheme.lightPurpleAccent,
+              color: Theme.of(this.context).colorScheme.lightPurpleAccent,
+              animationDuration: const Duration(milliseconds: 500),
+              backgroundColor: Colors.transparent,
+              height: 60,
+              items: icons,
+              onTap: (index) {
+                setState(() {
+                  selectedIndex = index;
+                });
+              },
+            ),
+          ),
+        ]),
+      );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,9 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
-import 'package:frosh_week_2t1/src/pages/schedule_page.dart';
-import 'package:frosh_week_2t1/src/pages/login_page.dart';
-import 'src/pages/home_page.dart';
-import 'src/pages/notifications_page.dart';
-import 'src/pages/resources_page.dart';
-import 'src/pages/leaders_page.dart';
+import 'framework.dart';
 import 'src/colors.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:curved_navigation_bar/curved_navigation_bar.dart';
-import 'package:animations/animations.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart' as fss;
@@ -20,6 +13,7 @@ import "package:intl/intl.dart";
 import 'dart:math';
 import '../src/functions.dart';
 import 'package:connectivity_plus/connectivity_plus.dart';
+
 
 
 Future<void> _messageHandler(RemoteMessage message) async {
@@ -41,11 +35,6 @@ Future<void> _messageHandler(RemoteMessage message) async {
       notifications.map((element) {
         return jsonEncode(element);
       }).toList());
-}
-
-Future<bool> hasNetwork() async {
-  ConnectivityResult connectivityResult = await (Connectivity().checkConnectivity());
-  return connectivityResult != ConnectivityResult.none;
 }
 
 Future<dynamic> loadData() async {
@@ -211,198 +200,5 @@ class _MyAppState extends State<MyApp> {
         dio: widget.dio,
         storage: widget.storage,
         loadedData: widget.loadedData);
-  }
-}
-
-class Framework extends StatefulWidget {
-  final bool isLoggedIn;
-  final Dio dio;
-  final fss.FlutterSecureStorage storage;
-  final dynamic loadedData;
-
-  const Framework({
-    Key? key,
-    required this.isLoggedIn,
-    required this.dio,
-    required this.storage,
-    required this.loadedData,
-  }) : super(key: key);
-
-  @override
-  FrameworkState createState() => FrameworkState();
-}
-
-class FrameworkState extends State<Framework> {
-  late PageController pageController;
-  int selectedIndex = 0;
-  late bool _loggedIn;
-  String froshName = "";
-  String froshGroup = "";
-  String froshId = "";
-  String discipline = "";
-  String shirtSize = "";
-
-  Future<void> setLoggedIn(bool login) async {
-    setState(() {
-      _loggedIn = login;
-    });
-    if (login) {
-      await getCurrentUser();
-    }
-  }
-
-  Future<bool> getCurrentUser() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    if (await hasNetwork()) {
-      print("Has network");
-      String? cookie = await widget.storage.read(key: 'cookie');
-      Response res = await widget.dio
-          .get('https://www.orientation.skule.ca/users/current',
-              options: Options(headers: {"cookie": cookie}))
-          .catchError((error) {
-        setState(() {
-          _loggedIn = false;
-        });
-      });
-      setState(() {
-        froshName = res.data["preferredName"];
-        froshGroup = res.data["froshGroup"];
-        froshId = res.data["_id"];
-        discipline = res.data["discipline"];
-        shirtSize = res.data["shirtSize"];
-      });
-      await prefs.setStringList(
-          'froshData', [froshName, froshGroup, froshId, discipline, shirtSize]);
-      return true;
-    } else {
-      List<String>? froshData = prefs.getStringList("froshData");
-      setState(() {
-        froshName = froshData![0];
-        froshGroup = froshData[1];
-        froshId = froshData[2];
-        discipline = froshData[3];
-        shirtSize = froshData[4];
-      });
-      return true;
-    }
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    pageController = PageController(initialPage: selectedIndex);
-    setLoggedIn(widget.isLoggedIn);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    bool leaderAccount = true;
-
-    List<Widget> pages = [
-      HomePage(
-        froshName: froshName,
-        froshGroup: froshGroup,
-        froshId: froshId,
-        discipline: discipline,
-        shirtSize: shirtSize,
-        froshScheduleData: widget.loadedData["scheduleJSON"]["lambda"],
-        welcomeMessage: widget.loadedData["welcomeMessage"],
-      ),
-      SchedulePage(data: widget.loadedData["scheduleJSON"]["lambda"]),
-      NotificationsPageParse(),
-      ResourcesPageParse(),
-    ];
-    List<Widget> icons = [
-      Icon(
-        Icons.home,
-        size: 30,
-        color: Colors.white,
-      ),
-      Icon(
-        Icons.event,
-        size: 30,
-        color: Colors.white,
-      ),
-      Icon(
-        Icons.notifications,
-        size: 30,
-        color: Colors.white,
-      ),
-      Icon(
-        Icons.book,
-        size: 30,
-        color: Colors.white,
-      ),
-    ];
-
-    if (leaderAccount) {
-      pages.add(LeadersPage());
-      icons.add(
-        Icon(
-          Icons.people,
-          size: 30,
-          color: Colors.white,
-        ),
-      );
-    }
-
-    if (!_loggedIn) {
-      return Scaffold(
-          body: LoginPage(
-              dio: widget.dio,
-              setLoggedIn: setLoggedIn,
-              storage: widget.storage));
-    } else
-      return Scaffold(
-        body: Stack(children: [
-          PageTransitionSwitcher(
-            transitionBuilder: (
-              child,
-              animation,
-              secondaryAnimation,
-            ) {
-              return FadeThroughTransition(
-                animation: animation,
-                secondaryAnimation: secondaryAnimation,
-                child: child,
-              );
-            },
-            child: pages[selectedIndex],
-          ),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Container(
-              height: 80,
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment(0, 0.8),
-                  colors: [
-                    Theme.of(this.context).canvasColor.withOpacity(0),
-                    Theme.of(this.context).colorScheme.lightPurpleAccent,
-                  ],
-                ),
-              ),
-            ),
-          ),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: CurvedNavigationBar(
-              buttonBackgroundColor:
-                  Theme.of(this.context).colorScheme.lightPurpleAccent,
-              color: Theme.of(this.context).colorScheme.lightPurpleAccent,
-              animationDuration: const Duration(milliseconds: 500),
-              backgroundColor: Colors.transparent,
-              height: 60,
-              items: icons,
-              onTap: (index) {
-                setState(() {
-                  selectedIndex = index;
-                });
-              },
-            ),
-          ),
-        ]),
-      );
   }
 }
