7e334b8c8dfa053a196f05eb6230fcc7880cfbd8
jameskokoska <jameskokoska@users.noreply.github.com>
2021-08-25 13:56:52 -0400

    added fancy background animations

--- lib/src/pages/login_page.dart
+++ lib/src/pages/login_page.dart
@@ -9,6 +9,7 @@ import 'package:url_launcher/url_launcher.dart';
 import "package:dio/dio.dart";
 import '../colors.dart';
 import "../functions.dart";
+import 'package:simple_animations/simple_animations.dart';
 
 class LoginPage extends StatefulWidget {
   final Dio dio;
@@ -161,133 +162,153 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: SingleChildScrollView(
-            child: Column(
+    return Stack(
       children: [
-        GestureDetector(
-          onTap: () {
-            triggerLeaderLogin();
-          },
-          child: MainHeader(
-            text: 'F!rosh Week',
-            textSmaller: "2T1",
-            icon: true,
-            topSpace: false,
-          ),
+        PlasmaRenderer(
+          type: PlasmaType.bubbles,
+          particles: 60,
+          color: Theme.of(context).colorScheme.animatedBubbles,
+          blur: 0.34,
+          size: 0.22,
+          speed: 0.46,
+          offset: 0,
+          blendMode: BlendMode.hardLight,
+          particleType: ParticleType.atlas,
+          variation1: 0.58,
+          variation2: 0.12,
+          variation3: 0.17,
+          rotation: 0,
         ),
-        Container(height: 24),
-        Align(
-            alignment: Alignment.topLeft,
-            child: Header(text: "Login", padding: true)),
-        Container(height: 15),
-        TextInput(
-            labelText: "Email",
-            onChanged: (text) {
-              setState(() {
-                email = text;
-              });
-            }),
-        Container(height: 18),
-        TextInput(
-            obscureText: true,
-            labelText: "Password",
-            onChanged: (text) {
-              setState(() {
-                password = text;
-              });
-            }),
-        Container(height: 18),
-        AnimatedSwitcher(
-          duration: Duration(milliseconds: 300),
-          switchInCurve: Curves.easeIn,
-          switchOutCurve: Curves.easeIn,
-          transitionBuilder: (Widget child, Animation<double> animation) {
-            return FadeTransition(
-              opacity: animation,
-              child: child,
-            );
-          },
-          child: loading
-              ? Container(
-                  width: 55,
-                  height: 55,
-                  child: CircularProgressIndicator(
-                    color: Theme.of(context).colorScheme.yellowAccent,
-                  ),
-                )
-              : AnimatedSwitcher(
-                  duration: Duration(milliseconds: 500),
-                  switchInCurve: Curves.easeInOutCubic,
-                  switchOutCurve: Curves.easeInOutCubic,
-                  transitionBuilder:
-                      (Widget child, Animation<double> animation) {
-                    return ScaleTransition(child: child, scale: animation);
-                  },
-                  child: loginLeaderType
-                      ? Row(
-                          key: ValueKey<bool>(loginLeaderType),
-                          children: [
-                            ButtonRegular(
-                                customWidth:
-                                    MediaQuery.of(context).size.width / 2 -
-                                        16 * 2,
-                                text: "Frosh Login",
-                                onPressed: () async {
-                                  await _login(
-                                    email: email,
-                                    password: password,
-                                    isFrosh: true,
-                                  );
-                                }),
-                            ButtonRegular(
-                                yellow: true,
-                                customWidth:
-                                    MediaQuery.of(context).size.width / 2 -
-                                        16 * 2,
-                                text: "Leader Login",
-                                onPressed: () async {
-                                  await _login(
-                                    email: email,
-                                    password: password,
-                                    isFrosh: false,
-                                  );
-                                }),
-                          ],
-                        )
-                      : ButtonRegular(
-                          key: ValueKey<bool>(loginLeaderType),
-                          text: "Login",
-                          onPressed: () async {
-                            await _login(
-                                email: email,
-                                password: password,
-                                isFrosh: true);
-                          })),
-        ),
-        Container(height: 15),
-        ButtonRegular(
-          text: "Signup",
-          onPressed: () async {
-            var url = "https://www.orientation.skule.ca/registration";
-            if (await canLaunch(url)) await launch(url);
-          },
-          outline: true,
-        ),
-        Padding(
-          padding: EdgeInsets.all(20),
-          child: AnimatedSwitcher(
-            duration: Duration(milliseconds: 400),
-            child: TextFont(
-              key: ValueKey<String>(error),
-              text: error,
-              fontSize: 16,
-              textColor: Theme.of(context).colorScheme.redAccent,
+        Center(
+            child: SingleChildScrollView(
+                child: Column(
+          children: [
+            Container(height: 70),
+            GestureDetector(
+              onTap: () {
+                triggerLeaderLogin();
+              },
+              child: MainHeader(
+                text: 'F!rosh Week',
+                textSmaller: "2T1",
+                icon: true,
+                topSpace: false,
+              ),
             ),
-          ),
-        ),
-        Container(height: 60)
+            Container(height: 24),
+            Align(
+                alignment: Alignment.topLeft,
+                child: Header(text: "Login", padding: true)),
+            Container(height: 15),
+            TextInput(
+                labelText: "Email",
+                onChanged: (text) {
+                  setState(() {
+                    email = text;
+                  });
+                }),
+            Container(height: 18),
+            TextInput(
+                obscureText: true,
+                labelText: "Password",
+                onChanged: (text) {
+                  setState(() {
+                    password = text;
+                  });
+                }),
+            Container(height: 18),
+            AnimatedSwitcher(
+              duration: Duration(milliseconds: 300),
+              switchInCurve: Curves.easeIn,
+              switchOutCurve: Curves.easeIn,
+              transitionBuilder: (Widget child, Animation<double> animation) {
+                return FadeTransition(
+                  opacity: animation,
+                  child: child,
+                );
+              },
+              child: loading
+                  ? Container(
+                      width: 55,
+                      height: 55,
+                      child: CircularProgressIndicator(
+                        color: Theme.of(context).colorScheme.yellowAccent,
+                      ),
+                    )
+                  : AnimatedSwitcher(
+                      duration: Duration(milliseconds: 500),
+                      switchInCurve: Curves.easeInOutCubic,
+                      switchOutCurve: Curves.easeInOutCubic,
+                      transitionBuilder:
+                          (Widget child, Animation<double> animation) {
+                        return ScaleTransition(child: child, scale: animation);
+                      },
+                      child: loginLeaderType
+                          ? Row(
+                              key: ValueKey<bool>(loginLeaderType),
+                              children: [
+                                ButtonRegular(
+                                    customWidth:
+                                        MediaQuery.of(context).size.width / 2 -
+                                            16 * 2,
+                                    text: "Frosh Login",
+                                    onPressed: () async {
+                                      await _login(
+                                        email: email,
+                                        password: password,
+                                        isFrosh: true,
+                                      );
+                                    }),
+                                ButtonRegular(
+                                    yellow: true,
+                                    customWidth:
+                                        MediaQuery.of(context).size.width / 2 -
+                                            16 * 2,
+                                    text: "Leader Login",
+                                    onPressed: () async {
+                                      await _login(
+                                        email: email,
+                                        password: password,
+                                        isFrosh: false,
+                                      );
+                                    }),
+                              ],
+                            )
+                          : ButtonRegular(
+                              key: ValueKey<bool>(loginLeaderType),
+                              text: "Login",
+                              onPressed: () async {
+                                await _login(
+                                    email: email,
+                                    password: password,
+                                    isFrosh: true);
+                              })),
+            ),
+            Container(height: 15),
+            ButtonRegular(
+              text: "Signup",
+              onPressed: () async {
+                var url = "https://www.orientation.skule.ca/registration";
+                if (await canLaunch(url)) await launch(url);
+              },
+              outline: true,
+            ),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: AnimatedSwitcher(
+                duration: Duration(milliseconds: 400),
+                child: TextFont(
+                  key: ValueKey<String>(error),
+                  text: error,
+                  fontSize: 16,
+                  textColor: Theme.of(context).colorScheme.redAccent,
+                ),
+              ),
+            ),
+            Container(height: 60)
+          ],
+        ))),
       ],
-    )));
+    );
   }
 }
